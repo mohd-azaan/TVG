d50a8c724b0b4449bda07f18dbecf50434a444c3
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2025-09-21 23:17:54 +0530

    feat: Enhance HeroSection styling for improved layout and responsiveness

--- src/screens/HomepageDesktop/sections/HeroSection/HeroSection.jsx
+++ src/screens/HomepageDesktop/sections/HeroSection/HeroSection.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const HeroSection = () => {
 	return (
-		<section className='relative w-full h-screen'>
+		<section className='relative w-full h-screen overflow-x-hidden'>
 			{/* Background image */}
 			<div
-				className='absolute inset-0 bg-cover bg-center bg-no-repeat'
+				className='absolute inset-0 bg-cover bg-right-top bg-no-repeat transform-gpu scale-[1.02] lg:scale-[1.0]'
 				style={{ backgroundImage: 'url(/hero.png)' }}
 			/>
 
@@ -18,15 +18,14 @@ export const HeroSection = () => {
 				<div className='h-20' />
 
 				{/* Left column: stacked headline + paragraph */}
-				<div className='relative z-40 mt-[10vh] lg:ml-[15vw] md:ml-[10vw] max-w-[560px]'>
-					<p className="[font-family:'Morganite',Helvetica] font-bold text-[28px] sm:text-[42px] lg:text-[54px] tracking-[1.10px] leading-[0.5] text-white">
-						<span className='text-white tracking-[0.61px]'>WELCOME TO </span>
-						<span className='text-[#00a14a] tracking-[0.61px]'>
+				<div className='relative z-40 mt-[10vh] lg:ml-[12vw] md:ml-[8vw] max-w-[520px] lg:max-w-[560px]'>
+					<p className="[font-family:'Morganite',Helvetica] font-bold text-[26px] sm:text-[34px] lg:text-[41px]  text-white">
+						<span className='text-white tracking-[0.03em]'>WELCOME TO </span>
+						<span className='text-[#00a14a] tracking-[0.03em]'>
 							THE VIRTUAL GREENS
 						</span>
 					</p>
-
-					<h1 className="mt-6 text-white font-morganite font-black text-[clamp(80px,8vw,120px)] leading-[0.9]">
+					<h1 className='mt-2 text-white font-morganite font-black text-[clamp(72px,8vw,112px)] leading-[0.90] tracking-[0.02em] lg:tracking-[0.03em]'>
 						WHERE GOLF
 						<br />
 						JUST GOT AN
@@ -34,7 +33,7 @@ export const HeroSection = () => {
 						UPGRADE
 					</h1>
 
-					<p className='mt-1 text-[#dedede] text-[24px] leading-7 lg:max-w-[530px] md:max-w-[500px] sm:max-w-[400px] font-TVG-typography-hero-text font-[number:var(--TVG-typography-hero-text-font-weight)]'>
+					<p className='mt-2 text-[#d8d8d8] text-[19px] leading-6 lg:max-w-[405px] md:max-w-[320px] sm:max-w-[300px] font-TVG-typography-hero-text font-[number:var(--TVG-typography-hero-text-font-weight)]'>
 						Riyadh&apos;s first indoor golf simulator lounge isn&apos;t just
 						about perfecting your swing. It&apos;s your new go-to for play,
 						practice, party, and everything in between. We&apos;ve blended
